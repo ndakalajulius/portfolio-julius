f6212164e98b35c51221c0f1140c83b3a5ee5845
ndakalajulius <juliusndakala98@gmail.com>
2024-10-30 16:47:29 +0300

    added styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css'; // Import your CSS file here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
